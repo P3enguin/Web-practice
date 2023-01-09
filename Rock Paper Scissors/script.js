10d7114a9b3cfdf340da581c22250139cfd5b7cd
Pengu <sellamB66@gmail.com>
2023-01-09 14:46:33 +0100

    Game script done

---
+++ Rock Paper Scissors/script.js
@@ -0,0 +1,27 @@
+                                                                                                                                      /* function that return a random from one of the three options */
+
+const option = ["rock", "paper", "scissors"];
+
+function getComputerChoice(){
+    return option[parseInt(Math.random() * 3)];
+}
+
+
+function playRound(playerSelection,computerSelection){
+        if (playerSelection.toLowerCase() === computerSelection)
+            return "It's a draw";
+        else if ((playerSelection.toLowerCase() === option[0] 
+            && computerSelection === option[1]) 
+            || (playerSelection.toLowerCase() === option[1] 
+            && computerSelection === option[0])
+            || (playerSelection.toLowerCase() === option[2] 
+            && computerSelection === option[1]) )
+            return ("You win! " + playerSelection.toLowerCase() + " beats " + computerSelection);
+        else
+            return ("You lost! " + computerSelection + " beats " + playerSelection.toLowerCase()); 
+}
+
+for (let i = 0 ; i < 5 ; i++)
+{
+    alert(playRound(prompt("Enter rock,paper or scissors"),getComputerChoice()));
+}
