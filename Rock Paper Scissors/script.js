5e3d0a89e6015de98b6cc09e435327c3233a6215
Pengu <sellamB66@gmail.com>
2023-01-09 15:49:48 +0100

    Add comments

--- Rock Paper Scissors/script.js
+++ Rock Paper Scissors/script.js
@@ -1,5 +1,4 @@
-                                                                                                                                      /* function that return a random from one of the three options */
-
+/* function that return a random from one of the three options */
 const option = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
@@ -10,6 +9,7 @@ function getComputerChoice(){
 function playRound(playerSelection,computerSelection){
         if (playerSelection.toLowerCase() === computerSelection)
             return "It's a draw";
+        /* Checking only the wining condition  */
         else if ((playerSelection.toLowerCase() === option[0] 
             && computerSelection === option[1]) 
             || (playerSelection.toLowerCase() === option[1] 
@@ -21,7 +21,7 @@ function playRound(playerSelection,computerSelection){
             return ("You lost! " + computerSelection + " beats " + playerSelection.toLowerCase()); 
 }
 
-for (let i = 0 ; i < 5 ; i++)
+for (let i = 0; i < 5; i++)
 {
     alert(playRound(prompt("Enter rock,paper or scissors"),getComputerChoice()));
 }
