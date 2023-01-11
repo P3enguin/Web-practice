9078313bd56790576ef4cc72760831a6f59adf05
Pengu <sellamB66@gmail.com>
2023-01-11 14:13:01 +0100

    Script modify to catch mouse events

--- Rock Paper Scissors/script.js
+++ Rock Paper Scissors/script.js
@@ -1,31 +1,36 @@
 /* function that return a random from one of the three options */
-// const option = ["rock", "paper", "scissors"];
+const option = ["rock", "paper", "scissors"];
 
-// function getComputerChoice(){
-//     return option[parseInt(Math.random() * 3)];
-// }
+function getComputerChoice(){
+    return option[parseInt(Math.random() * 3)];
+}
 
 
-// function playRound(playerSelection,computerSelection){
-//         if (playerSelection.toLowerCase() === computerSelection)
-//             return "It's a draw";
-//         /* Checking only the wining condition  */
-//         else if ((playerSelection.toLowerCase() === option[0] 
-//             && computerSelection === option[1]) 
-//             || (playerSelection.toLowerCase() === option[1] 
-//             && computerSelection === option[0])
-//             || (playerSelection.toLowerCase() === option[2] 
-//             && computerSelection === option[1]) )
-//             return ("You win! " + playerSelection.toLowerCase() + " beats " + computerSelection);
-//         else
-//             return ("You lost! " + computerSelection + " beats " + playerSelection.toLowerCase()); 
-// }
+function playRound(playerSelection,computerSelection){
+        if (playerSelection.toLowerCase() === computerSelection)
+            return "It's a draw";
 
-// for (let i = 0; i < 5; i++)
-// {
-//     alert(playRound(prompt("Enter rock,paper or scissors"),getComputerChoice()));
-// }
+        /* Checking only the wining condition  */
+        else if ((playerSelection.toLowerCase() === option[0] 
+            && computerSelection === option[1]) 
+            || (playerSelection.toLowerCase() === option[1] 
+            && computerSelection === option[0])
+            || (playerSelection.toLowerCase() === option[2] 
+            && computerSelection === option[1]) )
+            return ("You win! " + playerSelection.toLowerCase() + " beats " + computerSelection);
+        else
+            return ("You lost! " + computerSelection + " beats " + playerSelection.toLowerCase()); 
+}
 
-window.addEventListener('click',function(e) {
-        console.log(e);
-});
+function announceResult(e) {
+    let computerChoice = getComputerChoice();
+    let par = document.querySelector('.computer');
+    par.innerHTML = "computer";
+    // playRound(e.classList.value,computerChoice);
+
+}
+
+
+const buttons=document.querySelectorAll('button');
+
+buttons.forEach(button => button.addEventListener('click',announceResult));
