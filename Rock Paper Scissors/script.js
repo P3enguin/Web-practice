7d7e89256be85199e2dbbaed5aa09b65e8119b4a
Pengu <sellamB66@gmail.com>
2023-01-11 13:15:14 +0100

    Add style.css : - Buttons for rock-paper-scissor - Fonts of each button and for the page

--- Rock Paper Scissors/script.js
+++ Rock Paper Scissors/script.js
@@ -1,27 +1,31 @@
 /* function that return a random from one of the three options */
-const option = ["rock", "paper", "scissors"];
+// const option = ["rock", "paper", "scissors"];
 
-function getComputerChoice(){
-    return option[parseInt(Math.random() * 3)];
-}
+// function getComputerChoice(){
+//     return option[parseInt(Math.random() * 3)];
+// }
 
 
-function playRound(playerSelection,computerSelection){
-        if (playerSelection.toLowerCase() === computerSelection)
-            return "It's a draw";
-        /* Checking only the wining condition  */
-        else if ((playerSelection.toLowerCase() === option[0] 
-            && computerSelection === option[1]) 
-            || (playerSelection.toLowerCase() === option[1] 
-            && computerSelection === option[0])
-            || (playerSelection.toLowerCase() === option[2] 
-            && computerSelection === option[1]) )
-            return ("You win! " + playerSelection.toLowerCase() + " beats " + computerSelection);
-        else
-            return ("You lost! " + computerSelection + " beats " + playerSelection.toLowerCase()); 
-}
+// function playRound(playerSelection,computerSelection){
+//         if (playerSelection.toLowerCase() === computerSelection)
+//             return "It's a draw";
+//         /* Checking only the wining condition  */
+//         else if ((playerSelection.toLowerCase() === option[0] 
+//             && computerSelection === option[1]) 
+//             || (playerSelection.toLowerCase() === option[1] 
+//             && computerSelection === option[0])
+//             || (playerSelection.toLowerCase() === option[2] 
+//             && computerSelection === option[1]) )
+//             return ("You win! " + playerSelection.toLowerCase() + " beats " + computerSelection);
+//         else
+//             return ("You lost! " + computerSelection + " beats " + playerSelection.toLowerCase()); 
+// }
 
-for (let i = 0; i < 5; i++)
-{
-    alert(playRound(prompt("Enter rock,paper or scissors"),getComputerChoice()));
-}
+// for (let i = 0; i < 5; i++)
+// {
+//     alert(playRound(prompt("Enter rock,paper or scissors"),getComputerChoice()));
+// }
+
+window.addEventListener('click',function(e) {
+        console.log(e);
+});
