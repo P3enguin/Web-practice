dd93e63985cadb44c1520aead6f3501daa11c5e6
Pengu <sellamB66@gmail.com>
2023-01-13 08:27:58 +0100

    Use grid css to make the drawing grid

--- Etch-A-Sketch/grids.js
+++ Etch-A-Sketch/grids.js
@@ -1,8 +1,20 @@
-let grid = document.querySelector('.container');
 
-for (let i = 0 ; i < 256 ; i++)
+
+function drawGrid(rows,columns)
 {
-    let elem = document.createElement('div');
-    elem.classList.add('square');
-    grid.appendChild(elem);
+    let grid = document.querySelector('.container');
+
+    grid.style.setProperty('--columns-numbers',columns);
+    grid.style.setProperty('--row-numbers',rows);
+    
+    for (let i = 0 ; i < rows*columns ; i++)
+    {
+        let elem = document.createElement('div');
+        elem.classList.add('square');
+        grid.appendChild(elem);
+    }
 }
+    
+
+
+drawGrid(16,16);
