d5a61b15622cf437e9b35b0dbab9b494ca4d44c5
Pengu <sellamB66@gmail.com>
2023-01-16 13:04:09 +0100

    First hello world in ReactJS

---
+++ ReactApp/react-app/src/hello.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+
+class Hello extends Component {
+    state = {  } 
+    render() { 
+        return (<h1>Hello world</h1>);
+    }
+}
+ 
+export default Hello;
